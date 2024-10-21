ce4462220127597f646b37977f962219c074b89e
Michael Nguyen <ngxphong03@gmail.com>
2024-10-21 15:50:48 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,30 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        sm: "540px",
+        // => @media (min-width: 540px) { ... }
+
+        md: "720px",
+        // => @media (min-width: 720px) { ... }
+
+        lg: "960px",
+        // => @media (min-width: 960px) { ... }
+
+        xl: "1140px",
+        // => @media (min-width: 1140px) { ... }
+
+        "2xl": "1320px",
+        // => @media (min-width: 1320px) { ... }
+      },
+      boxShadow: {
+        box: "rgba(0, 0, 0, 0.15) 0px 5px 15px 0px",
+      },
+      textColor: {
+        primary: "rgb(251, 146, 60)",
+      },
     },
   },
   plugins: [],
+  darkMode: "class",
 };
